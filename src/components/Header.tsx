856dc9aae24fb059ab840734415b858e2ca53e92
Alexis Langlet <alexis.langlet@etu.umontpellier.fr>
2023-12-08 04:11:26 +0100

    fix names

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Header() {
   const items = [
     {
-      title: "Acceuil",
+      title: "Accueil",
       link: "/",
     },
     {
